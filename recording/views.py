d0924d599410421814d01282e1bc873846c0d97b
Yi <renyi1217@gmail.com>
2017-01-28 21:22:05 +0800

    added basic templates
    the views “/recording/” and “administration” now use the template of
    the admin app
    
    TODO: make the name of the logged-in user appear on the upper right as
    in the admin app

--- recording/views.py
+++ recording/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    # return HttpResponse("Hello World!")
+    return render(request, 'base_site.html', {'content': 'Hello recording'})

41c61aa9ef311df234b2a7a4273cdda5dec48d51
asdetrefle <qjchv@albatros>
2017-02-08 23:37:30 +0800

    The trick to exec a file in django project is...

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
-from django.conf import settings
-settings.configure()
+#run this code in django shell using execfile where everything is configued.
+
 from schedule.models import MatchWeek, Season
 from datetime import timedelta
 
@@ -8,8 +8,8 @@ def create_matchweek():
     season1 = Season.objects.get(pk=1)
     for i in range(2,15):
         last_week = MatchWeek.objects.get(pk=(i-1))
-        end_date = last_week + timedelta(days=7)
-        this_week = MatchWeek(week_number=i, Season=season1, start_date=last_week.end_date, end_date=end_date)
+        end_date = last_week.end_date + timedelta(days=7)
+        this_week = MatchWeek(week_number=i, season=season1, start_date=last_week.end_date, end_date=end_date)
         this_week.save()
 
     return

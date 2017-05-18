1cc138e370881d9e76b4a6b7a6f3b72d846acb46
asdetrefle <qjchv@albatros>
2017-05-18 23:47:06 +0800

    postgres try1

--- pooldb/settings.py
+++ pooldb/settings.py
@@ -86,12 +86,22 @@ WSGI_APPLICATION = 'pooldb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pooldb'
+    }
+}
+
 
 
 # Password validation

4a93dbe8488d8ead5c283ac449ce1a5d6b22f810
asdetrefle <qjchv@albatros>
2017-05-23 00:17:22 +0800

    add handicap to initialize 2

--- pooldb/settings.py
+++ pooldb/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')_0p!f-dpekqt5oe^)g*5sv)p9clc(6+zhmd&8w05x44)lxthg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

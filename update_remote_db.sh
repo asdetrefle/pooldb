82481fe0d605181ca1535f9a7c960a2fd62264b1
asdetrefle <qjchv@albatros>
2017-05-22 23:29:27 +0800

    add handicap to initalize page

--- update_remote_db.sh
+++ update_remote_db.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 # drop remote db
-heroku pg:reset DATABASE_URL --app pnh --pnh
-heroku pg:push
+heroku pg:reset DATABASE_URL --app pnh --confirm pnh
+heroku pg:push pooldb DATABASE_URL --app pnh
